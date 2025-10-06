bc7524347a621a9435437dd81cfcad6f1a8aee15
Marzi <77510439+marzi1986@users.noreply.github.com>
2025-10-06 20:24:01 +0330

    monday

--- js/code.js
+++ js/code.js
@@ -7,13 +7,19 @@ console.dir(products);
 const buttons=document.querySelectorAll(".filter");
 console.dir(buttons);
 
+const searchPrice= document.getElementById("search-price");
+console.dir(searchPrice);
+
+const btnSearchPrice= searchPrice.querySelector("button");
+console.dir(btnSearchPrice);
+
 //Search Name Input Text
 const searchNameHandler=(event)=>{
-    searchNameText=event.target.value.toLowerCase().trim();
+   const searchNameText=event.target.value.toLowerCase().trim();
 
     products.forEach((product)=>{
         const productName= product.children[1].innerText.toLowerCase().trim();
-        console.log(productName);
+        
         if (productName.includes(searchNameText))
             product.style.display="block"
         else
@@ -22,7 +28,6 @@ const searchNameHandler=(event)=>{
             })
 
 }
-searchName.addEventListener("keyup", searchNameHandler);
 
 //Button Category
 
@@ -40,23 +45,46 @@ const filterButtonHandler=(event)=>{
 
     products.forEach((product=>{
         const productCategory= product.dataset.category;
-    console.log(productCategory);
-
+    
      if(buttonUser === productCategory || buttonUser==="All")
         product.style.display="block";
     else
         product.style.display="none";
 
     }))
-    
-
-   
+      
 }
+
+//Price Search
+const searchPriceHandler=(event)=>{
+   const txtSearchPrice= +event.target.parentElement.children[0].value;
+    console.dir(txtSearchPrice);
+
+    products.forEach((product)=>{
+        const productPrice= product.children[2].innerText;
+        const fProductPrice= +productPrice.split(" ")[1];
+       console.log(fProductPrice);
+
+       if(txtSearchPrice === fProductPrice || !txtSearchPrice)
+        product.style.display="block";
+    else
+        product.style.display="none";
+    })
+
+}
+
+
+const windowHandlers= ()=>{
+searchName.addEventListener("keyup", searchNameHandler);
 
 buttons.forEach((button=>{
 
     button.addEventListener("click", filterButtonHandler);
 }))
 
-//change button theme
+btnSearchPrice.addEventListener("click", searchPriceHandler);
 
+
+};
+
+window.addEventListener("load", windowHandlers);
